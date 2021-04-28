1b157be7bd76e0a6be5f8938930666ed63b43422
SGKwon95 <ssunder7@naver.com>
2021-04-28 18:18:02 +0900

    feat : kakao hash key 확인 버튼 추가

--- eco100-app/app/src/main/java/com/mapo/eco100/navigation/CommunityViewFragment.kt
+++ eco100-app/app/src/main/java/com/mapo/eco100/navigation/CommunityViewFragment.kt
@@ -1,21 +1,39 @@
 package com.mapo.eco100.navigation
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.kakao.sdk.common.util.Utility
 import com.mapo.eco100.R
+import com.mapo.eco100.databinding.FragmentCommunityBinding
 
 class CommunityViewFragment : Fragment() {
+
+    private var _binding : FragmentCommunityBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_community, container, false)
+        _binding = FragmentCommunityBinding.inflate(inflater,container,false)
+        val view = binding.root
 
+        binding.button.setOnClickListener {
+            var keyHash = Utility.getKeyHash(container!!.context)
+            Log.d("kakaoKeyHash", keyHash)
+        }
+
+        return view
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
     companion object {
