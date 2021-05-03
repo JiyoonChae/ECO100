de67ec964bfa9719bc2ae4ce01e1aa7140cdf6fd
songhyeonju <shyunju7@naver.com>
2021-05-03 18:30:34 +0900

    add : 홈 drawable 추가 fix : 홈 viewpager 및 indicator 추가

---
+++ eco100-app/app/src/main/java/com/mapo/eco100/views/home/FragmentForViewPager.kt
@@ -0,0 +1,35 @@
+package com.mapo.eco100.views.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import com.mapo.eco100.R
+
+class FragmentForViewPager : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.home_viewpager_item, container, false)
+        val imageIV = view.findViewById<ImageView>(R.id.viewImage)
+        val bundle = arguments
+        bundle?.getInt("image", -1)?.let { imageIV.setImageResource(it) }
+        return view
+    }
+
+    companion object {
+
+        fun newInstance(imageSrc: Int): FragmentForViewPager {
+            val fragment = FragmentForViewPager()
+            with(Bundle()) {
+                putInt("image", imageSrc)
+                fragment.arguments = this
+            }
+            return fragment
+        }
+    }
+}
--- eco100-app/app/src/main/java/com/mapo/eco100/views/home/HomeViewFragment.kt
+++ eco100-app/app/src/main/java/com/mapo/eco100/views/home/HomeViewFragment.kt
@@ -4,17 +4,74 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.viewpager2.widget.ViewPager2
 import com.mapo.eco100.R
+import me.relex.circleindicator.CircleIndicator3
 
 class HomeViewFragment : Fragment() {
 
+    private lateinit var pager: ViewPager2
+    private lateinit var recycleGuideView: LinearLayout
+    private val pageNum = 4
+    private lateinit var pageIndicator: CircleIndicator3
+
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+
+        // viewpager
+        pager = view.findViewById(R.id.pager)
+        pager.orientation = ViewPager2.ORIENTATION_HORIZONTAL
+        pager.offscreenPageLimit = 4
+
+        // adapter
+        val pagerAdapter = HomeFragmentStateAdapter(this)
+        pagerAdapter.run {
+            appendFragment(FragmentForViewPager.newInstance(R.drawable.img_home_mid_volunteer))
+            appendFragment(FragmentForViewPager.newInstance(R.drawable.img_home_mid_dacu))
+            appendFragment(FragmentForViewPager.newInstance(R.drawable.img_home_mid_news))
+            appendFragment(FragmentForViewPager.newInstance(R.drawable.img_home_mid_challenge))
+
+            pager.adapter = pagerAdapter
+        }
+
+        // indicator
+        pageIndicator = view.findViewById(R.id.indicator)
+        pageIndicator.createIndicators(pageNum, 0)
+        pageIndicator.setViewPager(pager)
+
+        pager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageScrolled(
+                position: Int,
+                positionOffset: Float,
+                positionOffsetPixels: Int
+            ) {
+                super.onPageScrolled(position, positionOffset, positionOffsetPixels)
+                if (positionOffsetPixels == 0) {
+                    pager.currentItem = position
+                }
+            }
+
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                pageIndicator.animatePageSelected(position % pageNum)
+            }
+        })
+
+        // recycleGuideView
+        recycleGuideView = view.findViewById(R.id.recycleGuideView)
+        recycleGuideView.setOnClickListener {
+            Toast.makeText(context, "눌려따!", Toast.LENGTH_SHORT).show()
+        }
+
+        return view
     }
 
     companion object {
@@ -23,3 +80,4 @@ class HomeViewFragment : Fragment() {
         }
     }
 }
+
