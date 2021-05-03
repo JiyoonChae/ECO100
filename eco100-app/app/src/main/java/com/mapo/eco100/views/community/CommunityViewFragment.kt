fb1bfa0db9b55fa535f2177435b319eafa7d99df
SGKwon95 <ssunder7@naver.com>
2021-05-03 14:17:28 +0900

    viewpager -> viewpager2로 변경

--- eco100-app/app/src/main/java/com/mapo/eco100/views/community/CommunityViewFragment.kt
+++ eco100-app/app/src/main/java/com/mapo/eco100/views/community/CommunityViewFragment.kt
@@ -8,28 +8,31 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TableLayout
 import android.widget.Toast
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentStatePagerAdapter
+import androidx.fragment.app.*
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 import com.kakao.sdk.auth.model.OAuthToken
 import com.kakao.sdk.common.util.Utility
 import com.kakao.sdk.user.UserApiClient
 import com.mapo.eco100.R
 import com.mapo.eco100.databinding.FragmentCommunityBinding
+import com.mapo.eco100.navigation.BoardFragment
+import com.mapo.eco100.navigation.ChallengeFragment
 
 class CommunityViewFragment : Fragment() {
 
-    private var _binding : FragmentCommunityBinding? = null
+    private var _binding: FragmentCommunityBinding? = null
     private val binding get() = _binding!!
     private lateinit var parentContext: Context
 
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentCommunityBinding.inflate(inflater,container,false)
+        _binding = FragmentCommunityBinding.inflate(inflater, container, false)
         parentContext = container!!.context
 
         binding.button.setOnClickListener {
@@ -37,50 +40,20 @@ class CommunityViewFragment : Fragment() {
             Log.d("kakaoKeyHash", keyHash)
         }
 
-        binding.tabLayout.addTab(binding.tabLayout.newTab().setText("참여 게시판"))
-        binding.tabLayout.addTab(binding.tabLayout.newTab().setText("챌린지"))
+        val pagerAdapter = PagerAdapter(requireActivity())
+        pagerAdapter.addFragment(BoardFragment())
+        pagerAdapter.addFragment(ChallengeFragment())
 
-        val pagerAdapter = PagerAdapter(childFragmentManager)
         binding.viewPager.adapter = pagerAdapter
 
-        binding.tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener{
-            override fun onTabSelected(tab: TabLayout.Tab?) {
-                binding.viewPager.currentItem = tab!!.position
-            }
+        binding.viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {})
 
-            override fun onTabUnselected(tab: TabLayout.Tab?) {
-
-            }
-
-            override fun onTabReselected(tab: TabLayout.Tab?) {
-
-            }
-        })
-        binding.viewPager.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(binding.tabLayout))
+        val tabItems = listOf("참여 게시판", "챌린지")
+        TabLayoutMediator(binding.tabLayout, binding.viewPager) { tab, position ->
+            tab.text = tabItems[position]
+        }.attach()
 
         return binding.root
-    }
-
-    class PagerAdapter(
-        fragmentManager: FragmentManager
-    ) : FragmentStatePagerAdapter(fragmentManager) {
-        override fun getItem(position: Int): Fragment {
-            return when(position) {
-                0 -> {
-                    BoardFragment()
-                }
-                1 -> {
-                    ChallengeFragment()
-                }
-                else -> {
-                    BoardFragment()
-                }
-            }
-        }
-
-        override fun getCount(): Int {
-            return 2
-        }
     }
 
     override fun onDestroyView() {
@@ -92,5 +65,20 @@ class CommunityViewFragment : Fragment() {
         fun newInstance(): CommunityViewFragment {
             return CommunityViewFragment()
         }
+    }
+}
+
+class PagerAdapter(
+    fragmentActivity: FragmentActivity
+) : FragmentStateAdapter(fragmentActivity) {
+    var fragments: ArrayList<Fragment> = ArrayList()
+
+    override fun getItemCount() = fragments.size
+
+    override fun createFragment(position: Int) = fragments[position]
+
+    fun addFragment(fragment: Fragment) {
+        fragments.add(fragment)
+        notifyItemInserted(fragments.size - 1)
     }
 }
