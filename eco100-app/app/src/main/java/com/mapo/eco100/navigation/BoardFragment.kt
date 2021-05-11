979e253a22ee9221ce6b46a56620a6ea1f71fc4d
SGKwon95 <ssunder7@naver.com>
2021-05-11 15:48:16 +0900

    feat : 정렬 메뉴(바) 추가, 게시판 객체 클릭 시 실행되는 로직 변경

--- eco100-app/app/src/main/java/com/mapo/eco100/navigation/BoardFragment.kt
+++ eco100-app/app/src/main/java/com/mapo/eco100/navigation/BoardFragment.kt
@@ -4,23 +4,30 @@ import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.mapo.eco100.EnrollActivity
+import com.mapo.eco100.R
 import com.mapo.eco100.config.BOARD_CLICK
 import com.mapo.eco100.config.BOARD_ENROLL
 import com.mapo.eco100.databinding.FragmentBoardBinding
 import com.mapo.eco100.ShowBoardActivity
 import com.mapo.eco100.adapter.BoardAdapter
-import com.mapo.eco100.entity.board.Board
+import com.mapo.eco100.config.OkHttpClientObj
+import com.mapo.eco100.config.RetrofitObj
+import com.mapo.eco100.entity.board.BoardReadForm
+import com.mapo.eco100.entity.board.Boards
+import com.mapo.eco100.service.BoardService
 import com.mapo.eco100.viewmodel.BoardViewModel
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class BoardFragment : Fragment() {
 
@@ -29,7 +36,12 @@ class BoardFragment : Fragment() {
     private var _binding: FragmentBoardBinding? = null
     private val binding get() = _binding!!
     private lateinit var mainActivityContext: Context
-    private lateinit var boardAdapter : BoardAdapter
+    private lateinit var boardAdapter: BoardAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -40,10 +52,23 @@ class BoardFragment : Fragment() {
         mainActivityContext = requireContext()
 
         boardAdapter = BoardAdapter(
-            onClickItem = { board ->
-                val intent = Intent(mainActivityContext, ShowBoardActivity::class.java)
-                intent.putExtra("board_data", board)
-                startActivityForResult(intent, BOARD_CLICK)
+            onClickItem = { board_data ->
+                val service: BoardService =
+                    OkHttpClientObj.retrofit.build().create(BoardService::class.java)
+                service.read(board_data.board_id).enqueue(object : Callback<BoardReadForm> {
+                    override fun onResponse(
+                        call: Call<BoardReadForm>,
+                        response: Response<BoardReadForm>
+                    ) {
+                        val intent = Intent(mainActivityContext, ShowBoardActivity::class.java)
+                        intent.putExtra("board_data", response.body()!!)
+                        startActivityForResult(intent, BOARD_CLICK)
+                    }
+
+                    override fun onFailure(call: Call<BoardReadForm>, t: Throwable) {
+                        Toast.makeText(mainActivityContext, "글에 문제가 있습니다.", Toast.LENGTH_SHORT).show()
+                    }
+                })
             }
         )
 
@@ -61,14 +86,20 @@ class BoardFragment : Fragment() {
             adapter = boardAdapter
         }
 
-        binding.buttonEnroll.setOnClickListener {
-            startActivityForResult(Intent(mainActivityContext, EnrollActivity::class.java), BOARD_ENROLL)
+        binding.boardWriteButton.setOnClickListener {
+            startActivityForResult(
+                Intent(mainActivityContext, EnrollActivity::class.java),
+                BOARD_ENROLL
+            )
         }
 
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.toolBar.inflateMenu(R.menu.sort_boards)
+        binding.toolBar.overflowIcon = ContextCompat.getDrawable(mainActivityContext,R.drawable.ic_baseline_keyboard_arrow_down_24)
+
         viewModel.apply {
             boardsLiveData.observe(viewLifecycleOwner, Observer {
                 boardAdapter.updateBoards(it)
@@ -77,6 +108,22 @@ class BoardFragment : Fragment() {
             loadingLiveData.observe(viewLifecycleOwner, Observer { isLoading ->
                 binding.progressBar.visibility = if (isLoading) View.VISIBLE else View.GONE
             })
+        }
+
+        binding.toolBar.setOnMenuItemClickListener {
+            when(it.itemId) {
+                R.id.action_recent -> {
+                    boardAdapter.sortByRecent()
+                    binding.sortMenuText.text = "최근 순"
+                    true
+                }
+                R.id.action_likes -> {
+                    boardAdapter.sortByLikes()
+                    binding.sortMenuText.text = "좋아요 순"
+                    true
+                }
+                else -> false
+            }
         }
     }
 
@@ -90,7 +137,7 @@ class BoardFragment : Fragment() {
         when (requestCode) {
             BOARD_ENROLL -> {
                 Toast.makeText(mainActivityContext, "글 등록 성공", Toast.LENGTH_SHORT).show()
-                val responseBoard: Board = data!!.getSerializableExtra("created_board") as Board
+                val responseBoard: Boards = data!!.getSerializableExtra("created_board") as Boards
                 boardAdapter.addBoard(responseBoard)
             }
         }
@@ -101,4 +148,5 @@ class BoardFragment : Fragment() {
             return BoardFragment()
         }
     }
+
 }
