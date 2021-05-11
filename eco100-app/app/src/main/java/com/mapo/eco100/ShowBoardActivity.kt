d446d27a5866feac4d5e263567289a42a6d4bd09
SGKwon95 <ssunder7@naver.com>
2021-05-11 15:43:34 +0900

    update : 글 상세보기 이미지 사이즈 조절

--- eco100-app/app/src/main/java/com/mapo/eco100/ShowBoardActivity.kt
+++ eco100-app/app/src/main/java/com/mapo/eco100/ShowBoardActivity.kt
@@ -5,7 +5,7 @@ import android.os.Bundle
 import androidx.core.net.toUri
 import com.bumptech.glide.Glide
 import com.mapo.eco100.databinding.ActivityShowBoardBinding
-import com.mapo.eco100.entity.board.Board
+import com.mapo.eco100.entity.board.BoardReadForm
 
 class ShowBoardActivity : AppCompatActivity() {
 
@@ -15,12 +15,15 @@ class ShowBoardActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityShowBoardBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        val board : Board = intent.getSerializableExtra("board_data") as Board
+        val board : BoardReadForm = intent.getSerializableExtra("board_data") as BoardReadForm
         board?.let {
             binding.boardTitle.text = it.title
             binding.boardContents.text = it.contents
             it.image_url?.let { url_string ->
-                Glide.with(this).load(url_string.toUri()).into(binding.imageView2)
+                Glide.with(this)
+                    .load(url_string.toUri())
+                    .override(1440,1440)
+                    .into(binding.imageView2)
             }
             binding.boardWriter.text = it.user_nickname
         }
