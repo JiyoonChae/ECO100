ea67c36c2dd008de6740b1d69f17a8bc9fe9ac8c
soyounglee <star10147@naver.com>
2021-05-13 20:31:33 +0900

    add: ecobox FAQ row , Q mark 추가 feat :  ecobox FAQ 기능 추가

--- eco100-app/app/src/main/java/com/mapo/eco100/views/ecobox/navigation/EcoBoxRecycleGuideFragment.kt
+++ eco100-app/app/src/main/java/com/mapo/eco100/views/ecobox/navigation/EcoBoxRecycleGuideFragment.kt
@@ -1,75 +1,97 @@
 package com.mapo.eco100.views.ecobox.navigation
 
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.mapo.eco100.R
 import com.mapo.eco100.databinding.EcoboxFragmentRecycleGuideBinding
-import com.mapo.eco100.databinding.FragmentEcoboxBinding
 import com.mapo.eco100.views.ecobox.*
+
 
 class EcoBoxRecycleGuideFragment : Fragment() {
 
     private var _binding: EcoboxFragmentRecycleGuideBinding? = null
     private val binding get() = _binding!!
+    private lateinit var faqRecyclerView: RecyclerView
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
+    var data_category = arrayOf(
+        "종이류", "유리병", "페트병", "스티로폼류", "비닐류", "기타"
+    )
+
+    var data_question = arrayOf(
+        "핸드 타월은 어떻게 배출하나요?",
+        "깨진 유리는 어떻게 배출하나요?",
+        "페트병의 라벨과 뚜껑은 어떻게 배출하나요?",
+        "이물질이 묻어 있는 스티로폼은 재활용이 가능한가요?",
+        "재활용 표시가 없는 1회용 봉투, 에어캡(뽁뽁이), 세탁소 비닐 등도 재활용이 가능한가요?",
+        "감기 시럽, 소화제, 병원 처방 받은 가루약 등 의약품은 어떻게 배출하나요?"
+    )
+
+
+    override fun onCreateView(inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 
         _binding = EcoboxFragmentRecycleGuideBinding.inflate(inflater, container, false)
+        binding.searchIc.setColorFilter(Color.WHITE)
+        val adapter = FaqRecyclerAdapter()
+
+        faqRecyclerView = binding.ecoboxFaqRecyclerview
+        faqRecyclerView.adapter = adapter
+        faqRecyclerView.layoutManager = LinearLayoutManager(context)
+
 
         binding.btnPaper.setOnClickListener {
-            activity?.let{
+            activity?.let {
                 val intent = Intent(context, RecycleGuidePaperActivity::class.java)
                 startActivity(intent)
             }
         }
 
         binding.btnGlass.setOnClickListener {
-            activity?.let{
+            activity?.let {
                 val intent = Intent(context, RecycleGuideGlassActivity::class.java)
                 startActivity(intent)
+
             }
         }
 
         binding.btnPlastic.setOnClickListener {
-            activity?.let{
+            activity?.let {
                 val intent = Intent(context, RecycleGuidePlasticActivity::class.java)
                 startActivity(intent)
             }
         }
 
         binding.btnCan.setOnClickListener {
-            activity?.let{
+            activity?.let {
                 val intent = Intent(context, RecycleGuideCanActivity::class.java)
                 startActivity(intent)
             }
         }
 
         binding.btnVinyl.setOnClickListener {
-            activity?.let{
+            activity?.let {
                 val intent = Intent(context, RecycleGuideVinylActivity::class.java)
                 startActivity(intent)
             }
         }
 
         binding.btnStyrofoam.setOnClickListener {
-            activity?.let{
+            activity?.let {
                 val intent = Intent(context, RecycleGuideStyrofoamActivity::class.java)
                 startActivity(intent)
             }
         }
-
-
-
-
-
 
         return binding.root
 
@@ -80,4 +102,41 @@ class EcoBoxRecycleGuideFragment : Fragment() {
             return EcoBoxRecycleGuideFragment()
         }
     }
+
+    inner class FaqRecyclerAdapter : RecyclerView.Adapter<FaqRecyclerAdapter.FaqViewHolderClass>() {
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FaqViewHolderClass {
+            //항목으로 사용할 view객체 생성.
+            val itemView = layoutInflater.inflate(R.layout.row_ecobox_faq, null)
+            val holder = FaqViewHolderClass(itemView)
+
+
+            return holder
+
+        }
+
+        override fun onBindViewHolder(holder: FaqViewHolderClass, position: Int) {
+
+            holder.qmarkImageView.setImageResource(R.drawable.ic_ecobox_qmark)
+            holder.faqCategoryTextView.text = data_category[position]
+            holder.questionTextView.text = data_question[position]
+
+        }
+
+
+        override fun getItemCount(): Int {
+            return data_question.size
+        }
+
+
+        inner class FaqViewHolderClass(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+            val qmarkImageView = itemView.findViewById<ImageView>(R.id.qmarkImageView)
+            val faqCategoryTextView = itemView.findViewById<TextView>(R.id.faqCategoryTextView)
+            val questionTextView = itemView.findViewById<TextView>(R.id.questionTextView)
+
+        }
+
+    }
 }
+
