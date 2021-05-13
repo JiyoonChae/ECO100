b62556c5b729aba48adf78b77bd1d266de667a9d
songhyeonju <shyunju7@naver.com>
2021-05-13 13:51:38 +0900

    add : 맵 에코덕 아이콘 추가 fix : 맵 사용자 위치 설정

--- eco100-app/app/src/main/java/com/mapo/eco100/views/map/MapViewFragment.kt
+++ eco100-app/app/src/main/java/com/mapo/eco100/views/map/MapViewFragment.kt
@@ -1,17 +1,23 @@
 package com.mapo.eco100.views.map
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
 import android.location.Location
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
@@ -23,6 +29,8 @@ class MapViewFragment : Fragment(), OnMapReadyCallback, GoogleMap.OnMyLocationCl
 
     private var _binding: FragmentMapBinding? = null
     private val binding get() = _binding!!
+    private lateinit var bitmapDraw: BitmapDrawable
+    private lateinit var bitmap: Bitmap
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -33,6 +41,9 @@ class MapViewFragment : Fragment(), OnMapReadyCallback, GoogleMap.OnMyLocationCl
 
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+        bitmapDraw = resources.getDrawable(R.drawable.ic_map_ecoduck) as BitmapDrawable
+        bitmap = Bitmap.createScaledBitmap(bitmapDraw.bitmap, 112, 188, false)
 
         return binding.root
     }
@@ -46,10 +57,31 @@ class MapViewFragment : Fragment(), OnMapReadyCallback, GoogleMap.OnMyLocationCl
     override fun onMapReady(googleMap: GoogleMap) {
         val myLocation = LatLng(37.564009984338014, 126.90923531625434)
         val initCam: CameraPosition = CameraPosition.builder().target(myLocation).zoom(15f).build()
-
         googleMap.moveCamera(CameraUpdateFactory.newCameraPosition(initCam))
+
+        if (ActivityCompat.checkSelfPermission(
+                binding.root.context,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                binding.root.context,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return
+        }
+        googleMap.isMyLocationEnabled = true
+
         googleMap.addMarker(
-            MarkerOptions().position(myLocation).title("회사").snippet("돈버는 곳")
+            MarkerOptions().position(myLocation).title("회사").snippet("돈버는 곳").icon(
+                BitmapDescriptorFactory.fromBitmap(bitmap)
+            )
         )
         googleMap.moveCamera(CameraUpdateFactory.newLatLng(myLocation))
         googleMap.animateCamera(CameraUpdateFactory.zoomTo(10f))
