27c0a94d21d728c91c7103ecdcf74b2fdb1cf216
SGKwon95 <ssunder7@naver.com>
2021-05-18 18:43:06 +0900

    chore : 댓글 번호 추가, 변수 이름 변경

--- eco100-app/app/src/main/java/com/mapo/eco100/adapter/CommentAdapter.kt
+++ eco100-app/app/src/main/java/com/mapo/eco100/adapter/CommentAdapter.kt
@@ -7,25 +7,21 @@ import com.mapo.eco100.R
 import com.mapo.eco100.databinding.ItemCommentBinding
 import com.mapo.eco100.entity.comment.Comment
 
-class CommentAdapter : RecyclerView.Adapter<CommentAdapter.CommentViewHolder>() {
+class CommentAdapter(comments:List<Comment>) : RecyclerView.Adapter<CommentAdapter.CommentViewHolder>() {
 
-    private var comments: ArrayList<Comment> = ArrayList()
+    private var comments: List<Comment>
 
     init {
-        comments.add(Comment("홍길동","test 댓글","2021-05-11"))
-        comments.add(Comment("김아무개","test 댓글2","2021-05-12"))
-        comments.add(Comment("김삿갓","test 댓글3","2021-05-13"))
+        this.comments = comments
+//        comments.add(Comment("홍길동","test 댓글","2021-05-11"))
+//        comments.add(Comment("김아무개","test 댓글2","2021-05-12"))
+//        comments.add(Comment("김삿갓","test 댓글3","2021-05-13"))
     }
 
     class CommentViewHolder(val binding: ItemCommentBinding) : RecyclerView.ViewHolder(binding.root)
 
-    fun updateComments(items: ArrayList<Comment>) {
+    fun updateComments(items: List<Comment>) {
         comments = items
-        notifyDataSetChanged()
-    }
-
-    fun addComment(item: Comment) {
-        comments.add(0, item)
         notifyDataSetChanged()
     }
 
@@ -39,7 +35,7 @@ class CommentAdapter : RecyclerView.Adapter<CommentAdapter.CommentViewHolder>() 
     override fun onBindViewHolder(holder: CommentViewHolder, position: Int) {
         val comment: Comment = comments[position]
         holder.binding.apply {
-            commentNickname.text = comment.user_nickname
+            commentNickname.text = comment.writer
             commentDate.text = comment.date
             commentContents.text = comment.contents
         }
