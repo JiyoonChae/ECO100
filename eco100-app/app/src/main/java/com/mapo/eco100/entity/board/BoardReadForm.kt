f139d5e0a921aa5e50f7b375e6e0d3c2de807ea3
SGKwon95 <ssunder7@naver.com>
2021-05-18 18:42:40 +0900

    feat : 댓글 리스트랑 날짜 보이도록 수정

--- eco100-app/app/src/main/java/com/mapo/eco100/entity/board/BoardReadForm.kt
+++ eco100-app/app/src/main/java/com/mapo/eco100/entity/board/BoardReadForm.kt
@@ -1,14 +1,16 @@
 package com.mapo.eco100.entity.board
 
+import com.mapo.eco100.entity.comment.Comment
 import java.io.Serializable
-
 
 data class BoardReadForm (
     val board_id : Long,
     val user_nickname : String,
-    var title : String,
-    var contents : String,
-    var image_url : String?,
+    val title : String,
+    val contents : String,
+    val image_url : String?,
     var comments_cnt : Int,
-    var likes_cnt : Int
+    var likes_cnt : Int,
+    val comments : List<Comment>,
+    val date : String
 ) : Serializable
