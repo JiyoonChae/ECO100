f59633fb30262726102164baea586a5b40bf48bf
SGKwon95 <ssunder7@naver.com>
2021-05-20 02:54:05 +0900

    BOARD_EDIT, BOARD_DELETE 추가

--- eco100-app/app/src/main/java/com/mapo/eco100/config/RequestCode.kt
+++ eco100-app/app/src/main/java/com/mapo/eco100/config/RequestCode.kt
@@ -2,6 +2,8 @@ package com.mapo.eco100.config
 
 const val BOARD_CLICK = 1
 const val BOARD_ENROLL = 2
-const val PICK_PHOTO = 3
-const val REQUEST_PERMISSION = 4
-const val MY_PERMISSION_REQUEST_STORAGE = 5
+const val BOARD_EDIT = 3
+const val BOARD_DELETE = 4
+const val PICK_PHOTO = 5
+const val REQUEST_PERMISSION = 6
+const val MY_PERMISSION_REQUEST_STORAGE = 7
