5f27c9e624555012245bedb0263567ecf0c4deed
SGKwon95 <ssunder7@naver.com>
2021-05-20 02:55:21 +0900

    RESPONSE parameter 변경

--- eco100-app/app/src/main/java/com/mapo/eco100/entity/board/BoardReadForm.kt
+++ eco100-app/app/src/main/java/com/mapo/eco100/entity/board/BoardReadForm.kt
@@ -4,13 +4,15 @@ import com.mapo.eco100.entity.comment.Comment
 import java.io.Serializable
 
 data class BoardReadForm (
-    val board_id : Long,
-    val user_nickname : String,
+    val boardId : Long,
+    val userId: Long,
+    val nickname : String,
     val title : String,
     val contents : String,
-    val image_url : String?,
-    var comments_cnt : Int,
-    var likes_cnt : Int,
+    val imageUrl : String?,
     val comments : List<Comment>,
+    var commentsCnt : Int,
+    var likesCnt : Int,
+    var canClickLikes : Boolean,
     val date : String
 ) : Serializable
