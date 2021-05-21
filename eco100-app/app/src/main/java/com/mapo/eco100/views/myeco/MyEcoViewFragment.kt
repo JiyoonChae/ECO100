6cf992b6e91052f6cbdc29846e5398e896daaf64
songhyeonju <shyunju7@naver.com>
2021-05-21 15:00:17 +0900

    update : manifest & style merge file

--- eco100-app/app/src/main/java/com/mapo/eco100/views/myeco/MyEcoViewFragment.kt
+++ eco100-app/app/src/main/java/com/mapo/eco100/views/myeco/MyEcoViewFragment.kt
@@ -5,10 +5,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.mapo.eco100.R
-import com.mapo.eco100.views.MainActivity
 
 class MyEcoViewFragment : Fragment() {
 
