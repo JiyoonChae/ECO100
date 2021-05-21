c8d9045f73fcb27bf06bdcade4e4c7c6bc8712c7
songhyeonju <shyunju7@naver.com>
2021-05-21 10:35:34 +0900

    fix : 스플래시 화면 변경 add : 스플래시 레이아웃 추가

--- eco100-app/app/src/main/java/com/mapo/eco100/views/splash/SplashActivity.kt
+++ eco100-app/app/src/main/java/com/mapo/eco100/views/splash/SplashActivity.kt
@@ -4,23 +4,34 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import com.mapo.eco100.R
+import com.mapo.eco100.databinding.ActivitySplashBinding
 import com.mapo.eco100.views.MainActivity
 
 
 class SplashActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashBinding
     private val handler = Handler(Looper.getMainLooper())
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        Glide.with(this).asGif().load(R.raw.splash_eco100).into(binding.splashImg)
+
         handler.postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 1500L)
 
-        }, 2000L)
 
-        val intent = Intent(this, MainActivity::class.java)
-        startActivity(intent)
-        finish()
     }
 
 }
