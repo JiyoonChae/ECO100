43398c00331248415668951571f2467ae90df4f0
JiyoonChae <71802752+JiyoonChae@users.noreply.github.com>
2021-05-25 13:34:00 +0900

    mypage > 나의 뱃지 보기 다이얼로그 추가, 스타일 및 drawable 추가

---
+++ eco100-app/app/src/main/java/com/mapo/eco100/views/myeco/BadgeDialogFragment.kt
@@ -0,0 +1,27 @@
+package com.mapo.eco100.views.myeco
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.mapo.eco100.R
+
+
+
+
+class BadgeDialogFragment : BottomSheetDialogFragment() {
+
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_badge_dialog, container, false)
+    }
+
+
+}
--- eco100-app/app/src/main/java/com/mapo/eco100/views/myeco/MyBadgeListActivity.kt
+++ eco100-app/app/src/main/java/com/mapo/eco100/views/myeco/MyBadgeListActivity.kt
@@ -1,12 +1,90 @@
 package com.mapo.eco100.views.myeco
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.mapo.eco100.R
+import com.mapo.eco100.databinding.ActivityMainBinding
+import com.mapo.eco100.databinding.ActivityMyBadgeListBinding
+import com.mapo.eco100.databinding.FragmentBadgeDialogBinding
 
 class MyBadgeListActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMyBadgeListBinding
+
+    @SuppressLint("ResourceType")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_my_badge_list)
+        binding = ActivityMyBadgeListBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val bottomDialog = BadgeDialogFragment()
+
+        val bottomDialogBinding = FragmentBadgeDialogBinding.inflate(layoutInflater)
+
+        binding.btn1.setOnClickListener {
+
+
+            bottomDialog.show(supportFragmentManager, bottomDialog.tag)
+        }
+
+        binding.btn2.setOnClickListener {
+
+
+            bottomDialog.show(supportFragmentManager, bottomDialog.tag)
+        }
+
+        binding.btn3.setOnClickListener {
+            bottomDialog.show(supportFragmentManager, bottomDialog.tag)
+        }
+
+        binding.btn4.setOnClickListener {
+            bottomDialog.show(supportFragmentManager, bottomDialog.tag)
+        }
+
+        binding.btn5.setOnClickListener {
+            bottomDialog.show(supportFragmentManager, bottomDialog.tag)
+        }
+
+        binding.btn6.setOnClickListener {
+            bottomDialog.show(supportFragmentManager, bottomDialog.tag)
+        }
+
+        binding.btn7.setOnClickListener {
+            bottomDialog.show(supportFragmentManager, bottomDialog.tag)
+        }
+
+        binding.btn8.setOnClickListener {
+            bottomDialog.show(supportFragmentManager, bottomDialog.tag)
+        }
+
+        binding.btn9.setOnClickListener {
+            bottomDialog.show(supportFragmentManager, bottomDialog.tag)
+        }
+
+        binding.btn10.setOnClickListener {
+            bottomDialog.show(supportFragmentManager, bottomDialog.tag)
+        }
+
+        binding.btn11.setOnClickListener {
+            bottomDialog.show(supportFragmentManager, bottomDialog.tag)
+        }
+
+        binding.btn12.setOnClickListener {
+            bottomDialog.show(supportFragmentManager, bottomDialog.tag)
+        }
+
+
     }
+
+    fun setData(fragment: Fragment, title:String): Fragment {
+        val bundle =Bundle()
+        bundle.putString("title", title)
+        fragment.arguments = bundle
+        return fragment
+    }
+
 }
+
