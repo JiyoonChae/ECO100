6aec19d736da460e0e16b964cc50717c224389c9
soyounglee <star10147@naver.com>
2021-05-25 15:06:05 +0900

    add : zeroshop 상세 faegment fix : 상세페이지 및 사이트 웹뷰 연결 update : string

--- eco100-app/app/src/main/java/com/mapo/eco100/views/ecobox/ZeroshopDetailActivity.kt
+++ eco100-app/app/src/main/java/com/mapo/eco100/views/ecobox/ZeroshopDetailActivity.kt
@@ -2,6 +2,11 @@ package com.mapo.eco100.views.ecobox
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
+import androidx.fragment.app.replace
+import com.bumptech.glide.Glide
+import com.mapo.eco100.R
 import com.mapo.eco100.databinding.ActivityZeroshopDetailBinding
 
 class ZeroshopDetailActivity : AppCompatActivity() {
@@ -14,5 +19,40 @@ class ZeroshopDetailActivity : AppCompatActivity() {
         binding = ActivityZeroshopDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val name = intent.getStringExtra("name")
+        val detailInfo = intent.getStringExtra("detailInfo")
+        val phoneNum = intent.getStringExtra("phoneNum")
+        val webUrl = intent.getStringExtra("webUrl")
+        val adress = intent.getStringExtra("adress")
+        val runInfo = intent.getStringExtra("runInfo")
+
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.zeroshop_container, ZeroshopDetailFragment.newInstance()
+                .apply {
+                    arguments = Bundle().apply {
+                        putString("name", name)
+                        putString("detailInfo", detailInfo)
+                        putString("phoneNum", phoneNum)
+                        putString("webUrl", webUrl)
+                        putString("adress", adress)
+                        putString("runInfo", runInfo)
+                    }
+                }
+            ).commit()
+
+    }
+
+    fun replaceFragment(fragment: Fragment) {
+        val webUrl = intent.getStringExtra("webUrl")
+
+        val ft: FragmentTransaction = supportFragmentManager.beginTransaction()
+        ft.replace(
+            R.id.zeroshop_container, fragment
+                .apply {
+                    arguments = Bundle().apply {
+                        putString("webUrl", webUrl)
+                    } }
+        ).commit()
     }
 }
